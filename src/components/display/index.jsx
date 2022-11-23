113ef24959d7d8af0d1c1c7ad885bce745a96c9c
yoshimitsu64 <yoshiyoshimitsu64@gmail.com>
2022-11-23 03:13:30 +0300

    fix: mod

--- src/components/display/index.jsx
+++ src/components/display/index.jsx
@@ -9,7 +9,6 @@ import {
 } from "./styled";
 
 function Display({ expression, result, previousExpression }) {
-  console.log(result);
   return (
     <StyledDisplayContainer>
       <StyledOutputField>
--- src/helpers/calculatorHelpers.js
+++ src/helpers/calculatorHelpers.js
@@ -45,74 +45,78 @@ function changeOperator(indexOfLasNumber, copyTemporaryExpresssionArray, pressed
 }
 
 export function tokenizeInput(input) {
-  var expression = input.replace(/\s/g, "");
+  try {
+    var expression = input.replace(/\s/g, "");
 
-  if (/[^0-9.0-9+\-/*^()]/g.test(expression)) return null;
+    if (/[^0-9.0-9+\-/*%^()]/g.test(expression)) return null;
 
-  if (
-    /[-*/^(]/.test(expression.charAt(expression.length - 1)) ||
-    /[*/^)]/.test(expression.charAt(0))
-  )
-    return null;
+    if (
+      /[-*/%^(]/.test(expression.charAt(expression.length - 1)) ||
+      /[*/%^)]/.test(expression.charAt(0))
+    )
+      return null;
 
-  var indexesForZeros = [];
-  for (let i = 0; i < expression.length; i++)
-    if (expression.charAt(i) === "(" && expression.charAt(i + 1) === "-")
-      indexesForZeros.push(i + 1);
+    var indexesForZeros = [];
+    for (let i = 0; i < expression.length; i++)
+      if (expression.charAt(i) === "(" && expression.charAt(i + 1) === "-")
+        indexesForZeros.push(i + 1);
 
-  for (let i = 0; i < indexesForZeros.length; i++) {
-    expression =
-      expression.slice(0, indexesForZeros[i]) + "0" + expression.slice(indexesForZeros[i]);
+    for (let i = 0; i < indexesForZeros.length; i++) {
+      expression =
+        expression.slice(0, indexesForZeros[i]) + "0" + expression.slice(indexesForZeros[i]);
+    }
+
+    if (expression.charAt(0) === "-") expression = "0" + expression;
+
+    let tokens = [];
+    let parentheses = 0;
+    let currentNumber = "";
+    let areWeInsideANumber = false;
+    let haveWeFoundDot = false;
+    let wasPreviousTokenOperator = false;
+
+    for (let i = 0; i < expression.length; i++) {
+      var a = expression.charAt(i);
+
+      if (areWeInsideANumber && haveWeFoundDot && a === ".") return null;
+
+      if (/[+\-/*%^]/.test(a)) {
+        if (wasPreviousTokenOperator) return null;
+
+        wasPreviousTokenOperator = true;
+      } else {
+        wasPreviousTokenOperator = false;
+      }
+
+      if (/[0-9]/.test(a)) {
+        areWeInsideANumber = true;
+        currentNumber += a;
+      } else if (/[+\-/*%^()]/.test(a)) {
+        areWeInsideANumber = false;
+        haveWeFoundDot = false;
+
+        if (currentNumber !== "") tokens.push(currentNumber);
+
+        currentNumber = "";
+
+        tokens.push(a);
+      }
+
+      if (a === ".") {
+        currentNumber += ".";
+      }
+
+      if (a === "(") parentheses++;
+      if (a === ")") parentheses--;
+    }
+
+    if (parentheses != 0) return null;
+
+    tokens.push(currentNumber);
+    return tokens;
+  } catch (e) {
+    console.log(e);
   }
-
-  if (expression.charAt(0) === "-") expression = "0" + expression;
-
-  let tokens = [];
-  let parentheses = 0;
-  let currentNumber = "";
-  let areWeInsideANumber = false;
-  let haveWeFoundDot = false;
-  let wasPreviousTokenOperator = false;
-
-  for (let i = 0; i < expression.length; i++) {
-    var a = expression.charAt(i);
-
-    if (areWeInsideANumber && haveWeFoundDot && a === ".") return null;
-
-    if (/[+\-/*^]/.test(a)) {
-      if (wasPreviousTokenOperator) return null;
-
-      wasPreviousTokenOperator = true;
-    } else {
-      wasPreviousTokenOperator = false;
-    }
-
-    if (/[0-9]/.test(a)) {
-      areWeInsideANumber = true;
-      currentNumber += a;
-    } else if (/[+\-/*^()]/.test(a)) {
-      areWeInsideANumber = false;
-      haveWeFoundDot = false;
-
-      if (currentNumber !== "") tokens.push(currentNumber);
-
-      currentNumber = "";
-
-      tokens.push(a);
-    }
-
-    if (a === ".") {
-      currentNumber += ".";
-    }
-
-    if (a === "(") parentheses++;
-    if (a === ")") parentheses--;
-  }
-
-  if (parentheses != 0) return null;
-
-  tokens.push(currentNumber);
-  return tokens;
 }
 
 export function validateExpression(value, expression, dispatch) {
